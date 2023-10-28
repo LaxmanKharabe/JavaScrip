65617727956211c86b42d2eb035ff9390c78e98f
Laxman <laxman31097@gmail.com>
2023-10-28 21:59:38 +0530

    final changes

--- Datatypes_Comparison/comparision.js
+++ Datatypes_Comparison/comparision.js
@@ -3,17 +3,17 @@
 // console.log(2 < 1); // false
 // console.log(2 == 1); // false
 // console.log(2 != 1); // true
-// console.log("2" > 0); // ">" operator convert the string into number. so that is why O/P will be true.
-// console.log("2" < 0); // ">" operator convert the string into number. so that is why O/P will be false.
-// console.log(null == 0); // "==" equality check operator does not convert null in number so that is why it will print false 
+// console.log("2" > 0); // JavaScript performs type coercion and convert the string into number. so that is why O/P will be true.
+// console.log("2" < 0); // JavaScript performs type coercion and convert the string into number. so that is why O/P will be false.
+// console.log(null == 0); // "==" equality check operator; null is considered loosely equal to undefined, but not zero. So it will be false.
 // console.log(null === 0); // "===" strict equality check operator; null and 0 have different data types, so that is why it will be false
-// console.log(null > 0); // greater than ">" operator convert null into number and treat it as zero [0]. So that is why it prints false.
-// console.log(null >= 0); // greater than or equal to ">="  operator convert null into number and treat it as zero [0]. So that is why it prints true.
-// console.log(null < 0); // Less than "<" operator convert null into number and treat it as zero [0]. So that is why it prints false.
-// console.log(null <= 0); // greater than or equal to "<=" operator convert null into number and treat it as zero [0]. So that is why it prints true.
-// console.log(undefined == 0); // undefined will not be converted into number so it will be false.
-// console.log(undefined > 0); // undefined will not be converted into number so it will be false
-// console.log(undefined < 0); // undefined will not be converted into number so it will be false
+// console.log(null > 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns false.
+// console.log(null >= 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns true.
+// console.log(null < 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns false.
+// console.log(null <= 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns true.
+// console.log(undefined == 0); /*  When comparing undefined and 0 using ==, it will perform type coercion,
+// console.log(undefined > 0);  JavaScript will try to make them the same type for the comparison. 
+// console.log(undefined < 0);  However, undefined is not loosely equal to 0. so it will be false */
 // console.log(typeof(1>"2"))// type will be boolean.
 console.log("2" > false); // "2" will be convertred into common data type 2 will be number and false is equal to 0; so O/P will be true.
 console.log("0" < true); //"0" will br convertred into common data type; 0 will be number and true is equal to 1; so O/P will be true.
