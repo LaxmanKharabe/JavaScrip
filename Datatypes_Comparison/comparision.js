34aee88b8564740c02b21383a9c27a49ec8e43b7
Laxman <laxman31097@gmail.com>
2023-10-28 22:21:16 +0530

    Final ++ changes

--- Datatypes_Comparison/comparision.js
+++ Datatypes_Comparison/comparision.js
@@ -11,9 +11,9 @@
 // console.log(null >= 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns true.
 // console.log(null < 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns false.
 // console.log(null <= 0); // While comparing null will be converted into number due to type coercion and treat it as zero [0]. So that is why it returns true.
-// console.log(undefined == 0); /*  When comparing undefined and 0 using ==, it will perform type coercion,
-// console.log(undefined > 0);  JavaScript will try to make them the same type for the comparison. 
-// console.log(undefined < 0);  However, undefined is not loosely equal to 0. so it will be false */
-// console.log(typeof(1>"2"))// type will be boolean.
+// console.log(undefined == 0); /*  When comparing undefined and 0 using ==, it will perform type coercion, JavaScript will try to make them the same type for the comparison. However, undefined is not loosely equal to 0. so it will be false  type will be boolean.*/
+// console.log(undefined < 0);
+// console.log(typeof(1>"2"))
+// console.log(undefined > 0); 
 console.log("2" > false); // "2" will be convertred into common data type 2 will be number and false is equal to 0; so O/P will be true.
 console.log("0" < true); //"0" will br convertred into common data type; 0 will be number and true is equal to 1; so O/P will be true.
