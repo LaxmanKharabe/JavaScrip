b818618a783cadd1052cf9b13ad2f0528395563b
LaxmanKharabe <laxman31097@gmail.com>
2024-02-22 22:34:15 +0530

    array methods practiced

--- Array Methods/forLoop.js
+++ Array Methods/forLoop.js
@@ -13,4 +13,49 @@ for (let index = 0; index < course.length; index++) {
     // result = `${result} ${course[index]} : ${fees[index]}\n`;
     result += `${course[index]} : ${fees[index]}\n`;
 }
-console.log(result);
+// console.log(result);
+
+
+for (let ind = 0; ind < course.length; ind++){
+    // console.log(course[ind])
+}
+course.forEach((element, index, arra)=>{
+    // console.log(index)
+})
+
+let arrayCol = [
+    {name: 'Laxman', height: 150},
+    {name: 'Amol', height: 160},
+    {name: 'Shankar', height: 170},
+    {name: 'Vijay', height: 170},
+    {name: 'Prakash', height: 170}
+]
+
+let personData = arrayCol.filter(filByHeight=>filByHeight.height>160);
+// console.log(personData.map(allItems=>allItems.name));
+
+
+// const arraY = new Array(5);
+// // arraY.map((ele, ind)=>console.log(ind));
+// arraY.push("Laxman");
+// arraY.push("Laxman");
+// arraY.push("Laxman");
+// arraY.push("Laxman");
+// arraY.push("Laxman");
+// arraY.push("Laxman");
+// arraY.push("AMol");
+// for (let Index = 0; Index < arraY.length; Index++) {
+//     console.log(arraY[Index]);
+// }
+const arraY = ["Laxman", 10, "sdfsaf"]
+const newArray = ["abc", "124",...arraY]
+console.log();
+for (let Index = 0; Index < arraY.length; Index++) {
+    // console.log(arraY[Index]);
+}
+
+const lastItems = arraY.pop();
+
+console.log(newArray);
+console.log(lastItems);
+console.log(arraY);
